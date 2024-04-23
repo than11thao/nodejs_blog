17f70a432dd1287db4027f92a7e3d30787216a16
Thân Thị Thảo <thanthao051102@gmail.com>
2024-04-23 13:49:33 +0700

    template engine

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const path = require('path')
+const express = require('express')
+const handlebars = require('express-handlebars').engine
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+
+// HTTP logger
+app.use(morgan('combined'))
+
+// Template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'))
+console.log(path.join(__dirname))
+
+app.get('/', (req, res) => {
+  return res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  return res.render('news');
+})
+
+// 127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
