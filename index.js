242100ff8c92c2a8f32e8bc01f8be9b2462ddbe8
Thân Thị Thảo <thanthao051102@gmail.com>
2024-04-23 11:52:20 +0700

    use library morgan

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const morgan = require('morgan')
+
+app.use(morgan('combined'))
 
 app.get('/', (req, res) => {
-    var a = 1;
-    var b = 2;
-    var c = a+b;
-  return res.send('Hello World!')
+  return res.send('123')
 })
 
 // 127.0.0.1 - localhost
