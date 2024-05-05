375468b5cc633acec987f22a1dc7b3f119532df5
Than Thi Thao <thanthao051102@gmail.com>
2024-05-05 22:58:17 +0700

    [CRUD]Read from DB

--- src/app/Controllers/SiteController.js
+++ src/app/Controllers/SiteController.js
@@ -1,10 +1,17 @@
 const Course = require('../models/Course');
+const { mutipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     // GET /news
-    index(req, res) {
+    index(req, res, next) {
         Course.find({})
-            .then((courses) => res.json(courses))
-            .catch((err) => res.status(400).json({ error: 'ERROR??' }));
+            .then((courses) => {
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                });
+            })
+            // .catch((err) => res.status(400).json({ error: 'ERROR??' }));
+            // .catch(err => next(err))
+            .catch(next);
     }
     // return res.render('home');
     // GET /news/:slug
--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const Course = new Schema({
     name: { type: String, maxLength: 255 },
     description: { type: String, maxLength: 600 },
-    description: { type: String, maxLength: 255 },
+    image: { type: String, maxLength: 255 },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now },
 });
