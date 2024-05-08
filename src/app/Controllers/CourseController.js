0e854e8e6eb800dbf091c2e6763a41204e6db3ea
Thân Thị Thảo <thanthao051102@gmail.com>
2024-05-08 16:14:46 +0700

    [CRUD] delete course

--- src/app/Controllers/CourseController.js
+++ src/app/Controllers/CourseController.js
@@ -50,6 +50,15 @@ class CourseController {
             })
             .catch(next);
     }
+
+    // DELETE /courses/:id
+    destroy(req, res, next) {
+        Course.deleteOne({ _id: req.params.id })
+            .then(() => {
+                res.redirect('back');
+            })
+            .catch(next);
+    }
 }
 
 module.exports = new CourseController();
